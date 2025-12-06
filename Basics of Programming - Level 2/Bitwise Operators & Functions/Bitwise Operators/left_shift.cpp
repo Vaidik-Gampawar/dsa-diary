73819284f020e95ee3b66eaf3c33b09ba437a840
Vaidik Mangesh Gampawar <vdk.codes@Vaidiks-MacBook-Air.local>
2025-12-07 00:24:15 +0530

    bitwise done week 2

---
+++ Basics of Programming - Level 2/Bitwise Operators & Functions/Bitwise Operators/left_shift.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << (25 << 1) << endl;
+    cout << (25 << 2) << endl;
+    cout << ((-100) << 2) << endl;
+    return 0;
+}
---
+++ Basics of Programming - Level 2/Bitwise Operators & Functions/Bitwise Operators/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // even or odd
+    /*
+    int n;
+    cin >> n;
+
+    if((n & 1) == 0){
+        cout << "EVEN" << endl;
+    }
+    else{
+        cout << "ODD" << endl;
+    }
+    */
+
+    //Find number of set bits in a number
+    /*
+    int n;
+    cin >> n;
+
+    int count = 0;
+
+    while(n > 0){
+        int bit = n & 1;
+        if(bit == 1){
+            count++;
+        }
+        n = n >> 1;
+    } 
+    cout << count << endl;
+    */
+    
+}
