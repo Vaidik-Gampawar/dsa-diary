c0b0e34399fdc8976f10075c4880eae393163436
Vaidik-Gampawar <vaidikgampawar@gmail.com>
2025-12-09 23:41:56 +0530

    Arrays startedgit add .

--- Arrays, Time & Space Complexity/Arrays/main.cpp
+++ Arrays, Time & Space Complexity/Arrays/main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
-    cout << "Hello" << endl;
+    int arr[5] = {10, 20, 30, 40, 50}; // Array Intialization
+    int arr2[4];
+    fill(arr2,arr2+4,23);
+
+    cout << arr2[0] << " " << arr2[1];
+    
+
+    
     return 0;
 }
